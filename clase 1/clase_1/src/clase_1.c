828afa991ef9257a6590140591fa3f248ef44f01
Nicolás Maximiliano Caruso <nico.caruso1992@outlook.com>
2022-03-30 14:35:59 -0300

    ejercicios clase 1

---
+++ clase 1/clase_1/src/clase_1.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : clase_1.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	var edad;
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
